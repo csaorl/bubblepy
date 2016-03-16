1f25e0ceb11ca0aad869ff174440dd9cc263ad57
Szymon Pyżalski <szymon@pythonista.net>
2016-03-16 10:35:42 +0100

    Support for bytes
    * decoding will return binary format (bytes in py3k or str in py2)
    * encoding is possible with strings, but non-ascii characters will cause
      exception

--- bubblepy.py
+++ bubblepy.py
@@ -1,6 +1,18 @@
 import six
 
 
+def get_byte(v):
+    """If argument is integer, check if it fits into one byte. One-byte strings
+    are first converted with ord()"""
+    if isinstance(v, int):
+        result = v
+    else:
+        result = ord(v)
+    if not 0 < result < 255:
+        raise ValueError('Non-ASCII character found. Encode your data')
+    return result
+
+
 class BubbleBabble(object):
     """ encodes or decodes to and from bubblebabble """
     def __init__(self):
@@ -21,7 +33,7 @@ class BubbleBabble(object):
                 )
                 break
 
-            byte1 = ord(src[i])
+            byte1 = get_byte(src[i])
             out += self.vowels[(((byte1 >> 6) & 3) + c) % 6]
             out += self.consonants[(byte1 >> 2) & 15]
             out += self.vowels[((byte1 & 3) + (c // 6)) % 6]
@@ -29,7 +41,7 @@ class BubbleBabble(object):
             if (i + 1) >= len(src):
                 break
 
-            byte2 = ord(src[i + 1])
+            byte2 = get_byte(src[i + 1])
             out += self.consonants[(byte2 >> 4) & 15]
             out += '-'
             out += self.consonants[byte2 & 15]
@@ -55,7 +67,7 @@ class BubbleBabble(object):
         src = src[1:-1]
         src = list(enumerate([src[x:x + 6] for x in range(0, len(src), 6)]))
         last_tuple = len(src) - 1
-        out = ''
+        out = bytes() 
 
         for k, tup in src:
             pos = k * 6
@@ -70,13 +82,12 @@ class BubbleBabble(object):
                         raise ValueError("corrupt string at offset %d (checksum)" % (pos + 2))
                 else:
                     byte = self._decode_3way_byte(tup[0], tup[1], tup[2], pos, c)
-                    out += chr(byte)
+                    out += six.int2byte(byte)
             else:
                     byte1 = self._decode_3way_byte(tup[0], tup[1], tup[2], pos, c)
                     byte2 = self._decode_2way_byte(tup[3], tup[5], pos)
 
-                    out += chr(byte1)
-                    out += chr(byte2)
+                    out += six.int2byte(byte1) + six.int2byte(byte2)
 
                     c = (c * 5 + byte1 * 7 + byte2) % 36
 
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import unittest
 
 from bubblepy import BubbleBabble
@@ -17,12 +18,32 @@ class TestBB(unittest.TestCase):
         ('Pineapple', 'xigak-nyryk-humil-bosek-sonax'),
     }
 
+    NON_ASCII = u'Pchnąć w tę łódź jeża lub ośm skrzyń fig'
+
     def test_encode(self):
         """Test encoding valid values"""
         for src, encoded in self.TESTS:
             self.assertEqual(self.BB.encode(src), encoded)
 
+    def test_encode_bytes(self):
+        """Test encoding valid values converted to bytes"""
+        for src, encoded in self.TESTS:
+            self.assertEqual(self.BB.encode(src.encode('utf-8')), encoded)
+
+    def test_encode_nonascii(self):
+        """Non-ASCII characters should raise errors"""
+        with self.assertRaises(ValueError):
+            self.BB.encode(self.NON_ASCII)
+
+    def test_encode_utf8(self):
+        """Encoding text with UTF-8 should allow to use bubble babble"""
+        utf = self.NON_ASCII.encode('utf-8')
+        bb = self.BB.encode(utf)
+        decoded = self.BB.decode(bb).decode('utf-8')
+        self.assertEqual(decoded, self.NON_ASCII)
+
+
     def test_decode(self):
         """Test decoding valid values"""
         for src, encoded in self.TESTS:
-            self.assertEqual(self.BB.decode(encoded), src)
+            self.assertEqual(self.BB.decode(encoded).decode('utf-8'), src)
